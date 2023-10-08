1058549eed19482fc7158d9e8a7997e266fe5b0c
Dominik Sumer <domi.sumer@gmail.com>
2023-10-08 07:12:54 +0200

    remove unused file

--- mdx.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '*.mdx' {
-  let MDXComponent: (props) => JSX.Element;
-  export default MDXComponent;
-
-  export const metadata: {
-    title: string;
-    description: string;
-  };
-}
