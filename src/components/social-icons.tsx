73951c7614a5d223d837c3d8652db1e68e76c0d0
Dominik Sumer <domi.sumer@gmail.com>
2025-05-12 16:08:58 +0200

    updated social links

--- src/components/social-icons.tsx
+++ src/components/social-icons.tsx
@@ -1,78 +1,24 @@
-import { CgIndieHackers } from 'react-icons/cg';
-import { FaGithub, FaInstagram, FaLinkedin, FaTwitter } from 'react-icons/fa';
-import { css } from 'styled-system/css';
-import { Box, Flex, FlexProps } from 'styled-system/jsx';
+import { TbBrandBluesky, TbBrandGithub, TbBrandLinkedin, TbBrandThreads, TbBrandX } from 'react-icons/tb';
+import { Flex, FlexProps } from 'styled-system/jsx';
 import { Link } from './common/link';
 
 export const SocialIcons = (props: FlexProps) => {
   return (
-    <Flex align="center" gap={2} {...props}>
-      <Link href="https://twitter.com/dominiksumer" isExternal>
-        <FaTwitter
-          className={css({
-            width: '17px',
-            height: '17px',
-            color: '#1fa1f1',
-          })}
-        />
+    <Flex align="center" mt={4} gap={3} color="gray.600">
+      <Link href="https://x.com/dominiksumer" isExternal>
+        <TbBrandX size={20} />
       </Link>
-      <Link display="flex" href="https://www.linkedin.com/in/dominik-sumer/" isExternal pos="relative">
-        <Box
-          position="absolute"
-          left="0px"
-          top="0px"
-          width="14px"
-          height="14px"
-          bg="white"
-          transform="translate(2px, 2px)"
-        />
-        <FaLinkedin
-          className={css({
-            pos: 'relative',
-            width: '18px',
-            height: '18px',
-            color: '#0a66c2',
-          })}
-        />
+      <Link href="https://bsky.app/profile/dominiksumer.com" isExternal>
+        <TbBrandBluesky size={20} />
       </Link>
-      <Link href="https://www.indiehackers.com/dominiksumer" isExternal>
-        <CgIndieHackers
-          className={css({
-            mx: '-2px',
-            width: '20px',
-            height: '20px',
-            color: 'github',
-          })}
-        />
+      <Link href="https://www.linkedin.com/in/dominik-sumer/" isExternal>
+        <TbBrandLinkedin size={20} />
       </Link>
-      <Link
-        display="flex"
-        href="https://www.instagram.com/dominik.sumer/"
-        isExternal
-        flexShrink={0}
-        justifyContent="center"
-        alignItems="center"
-        width="17px"
-        height="17px"
-        rounded="4px"
-        bg="linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%)"
-      >
-        <FaInstagram
-          className={css({
-            width: '14px',
-            height: '14px',
-            color: 'white',
-          })}
-        />
+      <Link href="https://www.threads.com/@dominik.sumer" isExternal>
+        <TbBrandThreads size={20} />
       </Link>
       <Link href="https://github.com/dsumer" isExternal>
-        <FaGithub
-          className={css({
-            width: '18px',
-            height: '18px',
-            color: 'github',
-          })}
-        />
+        <TbBrandGithub size={20} />
       </Link>
     </Flex>
   );
