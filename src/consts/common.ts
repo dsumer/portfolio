46aa0b866eb1f865ef355124ec70f7c4aae7ec70
Dominik Sumer <domi.sumer@gmail.com>
2025-06-18 11:01:00 +0200

    changed pinned tweet

--- src/consts/common.ts
+++ src/consts/common.ts
@@ -1,4 +1,4 @@
 export const websiteUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : `http://localhost:${process.env.PORT || 3000}`;
-export const PINNED_TWEET_ID = '1922628742473822705';
+export const PINNED_TWEET_ID = '1934951974920995279';
